1382246e9c9e0f53e783a10415526538f8a344c7
nobody <nobody@localhost>
2023-04-16 04:01:37 +0100

    move prompt description out of handler logic

--- examples/mermaid/src/index.ts
+++ examples/mermaid/src/index.ts
@@ -28,26 +28,17 @@ router.get('/', MermaidRoute)
 router.get('/.well-known/ai-plugin.json', (request: Request) => {
   const host = request.headers.get('host')
   const openAPIUrl = `http://${host}/openapi.json`;
+
   console.log("using manifest", openAPIUrl);
+
   const pluginManifest = defineAIPluginManifest(
     {
-      description_for_human: pkg.description,
-      name_for_human: pkg.aiPlugin.name,
-      ...pkg.aiPlugin,
-      description_for_model: `
-      You should use this plugin when people want visualizations.
-
-The plugin takes in a mermaid diagram and renders it returning a link to the rendered image.
-The plugin expects a mermaid.js snippet as input.
-The snippet that it is passed should use fa v4 icons.
-The snippet should only contain the mermaid diagram and NOT the mermaid.js code block using \`\`\`mermaid\`\`\`.
-
-If you think the best diagram to represent the data is a mindmap:
-- Limit to 2 outer layers outside the root.
-
-Interpreting the response:
-When you get the response it will include an image url, you should render it inline.
-`
+      "description_for_human": "Render Mermaid Graphs",
+      "name_for_human": "Mermaid",
+      "logo_url": "https://assets.standardresume.co/image/upload/c_fill,w_392,h_392,f_auto,q_auto/dexa/accounts/lex",
+      "contact_email": "kirill2003de@gmail.com",
+      "legal_info_url": "https://example.com",
+      "description_for_model": DESCRIPTION_FOR_MODEL,
     },
     { openAPIUrl }
   )
@@ -67,7 +58,6 @@ router.all('*', () => new Response('404 Not Found...', { status: 200 }))
 export default {
   fetch: (request: Request, env: Env, ctx: ExecutionContext) => {
     if (request.method === 'OPTIONS') {
-      console.log("got a cors request");
       return new Response(null, {
         status: 200,
         headers: {
@@ -79,3 +69,18 @@ export default {
     return router.handle(request, env, ctx).then(corsify);
   }
 }
+
+const DESCRIPTION_FOR_MODEL = (
+`You should use this plugin when people want visualizations.
+
+The plugin takes in a mermaid diagram and renders it returning a link to the rendered image.
+The plugin expects a mermaid.js snippet as input.
+The snippet that it is passed should use fa v4 icons.
+The snippet should only contain the mermaid diagram and NOT the mermaid.js code block using \`\`\`mermaid\`\`\`.
+
+If you think the best diagram to represent the data is a mindmap:
+- Limit to 2 outer layers outside the root.
+
+Interpreting the response:
+When you get the response it will include an image url, you should render it inline.
+`);
