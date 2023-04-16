54894926efbcec12c550efffb582b69a982dde33
nobody <nobody@localhost>
2023-04-16 03:15:53 +0100

    localhost dev works on my machine

--- examples/mermaid/src/index.ts
+++ examples/mermaid/src/index.ts
@@ -1,5 +1,6 @@
 import { OpenAPIRouter } from '@cloudflare/itty-router-openapi'
 import { defineAIPluginManifest } from 'chatgpt-plugin'
+import { createCors } from 'itty-cors';
 
 import * as routes from './routes'
 import pkg from '../package.json'
@@ -17,22 +18,17 @@ const router = OpenAPIRouter({
   }
 })
 
-// CORS - Doesn't actualy work all that well
-router.options('*', (request: Request) => {
-  console.log(request.url)
-  return new Response(null, {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS'
-    }
-  })
-})
-
-router.get('/', routes.Mermaid)
+const { preflight, corsify } = createCors({ origins: ['*'] });
+router.all('*', preflight)
 
 // 1. Define the plugin manifest
+router.get('/', routes.Mermaid)
+//router.get('/preview.html', routes.previewHandle)
+
 router.get('/.well-known/ai-plugin.json', (request: Request) => {
   const host = request.headers.get('host')
+  const openAPIUrl = `http://${host}/openapi.json`;
+  console.log("using manifest", openAPIUrl);
   const pluginManifest = defineAIPluginManifest(
     {
       description_for_human: pkg.description,
@@ -53,14 +49,14 @@ Interpreting the response:
 When you get the response it will include an image url, you should render it inline.
 `
     },
-    { openAPIUrl: `https://${host}/openapi.json` }
+    { openAPIUrl }
+   // { openAPIUrl: `http://127.0.0.1:35243/openapi.json` }
   )
 
   console.log(pluginManifest)
 
   return new Response(JSON.stringify(pluginManifest, null, 2), {
     headers: {
-      'Access-Control-Allow-Origin': '*',
       'content-type': 'application/json;charset=UTF-8'
     }
   })
@@ -70,6 +66,19 @@ When you get the response it will include an image url, you should render it inl
 router.all('*', () => new Response('404 Not Found...', { status: 200 }))
 
 export default {
-  fetch: (request: Request, env: Env, ctx: ExecutionContext) =>
-    router.handle(request, env, ctx)
+  fetch: (request: Request, env: Env, ctx: ExecutionContext) => {
+    const host = request.headers.get('host')
+    console.log("================= host", host);
+    if (request.method === 'OPTIONS') {
+      console.log("we got a cors request");
+      return new Response(null, {
+        status: 200,
+        headers: {
+          'access-control-allow-origin': '*', 
+        } 
+      });
+    } 
+    console.log("we got other cors request", request.method);
+    return router.handle(request, env, ctx).then(corsify);
+  }
 }
