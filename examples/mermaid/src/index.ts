b72d8fd185ae5671c36faf533d338c94799709a3
Kirill Dubovitskiy <kirill2003de@gmail.com>
2023-04-16 06:38:08 -0700

    gpt-4

--- examples/mermaid/src/index.ts
+++ examples/mermaid/src/index.ts
@@ -77,7 +77,45 @@ export default {
   }
 }
 
-const DESCRIPTION_FOR_MODEL = `You should use this plugin when people want visualizations.
+const DESCRIPTION_FOR_MODEL = `
+You should use this plugin when people want visualizations.
+Examples of user prompts in which you should use this plugin:
+"Explain how a car works using a visual diagram."
+"Describe the process of making a car."
+"How does a car engine work?" 
+
+Use this plugin if a user asks about: how something works, explain about a concept or how something is made, summarize a book, describe a process, and other user prompts that can be summarized or explained in the format of a mindmap or a state diagram. Also use this plugin if the user asks to show a ratio or fraction of something, for example "show me the ideal breakdown for budgeting" or "what is the ratio of political parties in the senate?". Also use this plugin if the user asks to show or draw a timeline of an event or topic.
+
+How to create a request to the plugin API:
+Pass the user prompt as a query parameter to the reqeust.
+
+Example requests:
+{
+  query: "Explain how a car works using a visual diagram."
+}
+{
+  query: "How does the US government work?"
+}
+{
+  query: "Show me how a pen is structured."
+}
+
+
+Interpreting the API response:
+When you get the response it will include an image url, you should render it inline using ![alt text](image) syntax.
+You will also get a link as editDiagramOnline to edit the diagram, you should render it inline using [link text](url) syntax.
+Include the links only once, do not repeat the same link.
+`
+
+const DESCRIPTION_FOR_MODEL_LONGER = `
+You should use this plugin when people want visualizations.
+Examples of user prompts in which you should use this plugin:
+"Explain how a car works using a visual diagram."
+"Describe the process of making a car."
+"How does a car engine work?" 
+The plugin api takes in a mermaid code snippet and renders it returning a link to the rendered image, and a link to edit the diagram online.
+
+Use this plugin if a user asks about: how something works, explain about a concept or how something is made, summarize a book, describe a process, and other user prompts that can be summarized or explained in the format of a mindmap or a state diagram. Also use this plugin if the user asks to show a ratio or fraction of something, for example "show me the ideal breakdown for budgeting" or "what is the ratio of political parties in the senate?". Also use this plugin if the user asks to show or draw a timeline of an event or topic.
 
 The plugin takes in a mermaid diagram and renders it returning a link to the rendered image.
 The plugin expects a mermaid.js snippet as input.
@@ -87,7 +125,13 @@ The snippet should only contain the mermaid diagram and NOT the mermaid.js code 
 If you think the best diagram to represent the data is a mindmap:
 - Limit to 2 outer layers outside the root.
 
-Interpreting the response:
+How to create the API request to the plugin:
+- The plugin expects a mermaid.js snippet as mermaid parameter
+- It should be properly indentend, otherwise it will break
+
+Interpreting the API response:
 When you get the response it will include an image url, you should render it inline.
 You will also get a link as editDiagramOnline to edit the diagram, you should render it inline.
+You should not print the mermaid snippet in the response.
+You should keep your textual explanation short - the diagram is the main focus of the response.
 `
--- examples/mermaid/src/routes/Mermaid.ts
+++ examples/mermaid/src/routes/Mermaid.ts
@@ -4,7 +4,7 @@ import { isValidChatGPTIPAddress } from 'chatgpt-plugin'
 
 import * as types from '../types'
 import { omit } from '../utils'
-import  { saveShortLink } from './Shorten'
+import { saveShortLink } from './Shorten'
 
 function compressAndEncodeBase64(input: string) {
   // Convert the input string to a Uint8Array
@@ -37,47 +37,63 @@ function encodeBase64(input: string) {
   return base64Encoded
 }
 
+export interface Interaction {
+  role: string
+  content: string
+}
+
+export async function getGPTResponse(
+  userQuestion: string,
+  apiKey: string
+): Promise<string> {
+  const apiUrl = `https://api.openai.com/v1/chat/completions`
+
+  // TODO: Dominic suggests savings :D
+  // return "Good to hear bro";
+
+  // get env OPENAI_KEY, from .env
+
+  let chatRequest = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      model: 'gpt-4',
+      messages: [
+        {
+          role: 'system',
+          content: systemPrompt(userQuestion)
+        }
+      ],
+      temperature: 0.7
+    })
+  })
+
+  const response = await chatRequest.json()
+  let textReply = response.choices[0].message.content
+
+  return textReply
+}
+
+console.log(compressAndEncodeBase64('digraph G {Hello->World}'))
+
 export class MermaidRoute extends OpenAPIRoute {
   /// 2. Creates /openapi.json route under the hood. Injects this into gpt prompt to teach about how to use the plugin.
   static schema = {
     tags: ['Diagram', 'Mermaid'],
-    summary: `Taking in a diagram, renders it and returns a link to the rendered image. Select the diagram language from amongst BlockDiag, BPMN, Bytefield, C4 (with PlantUML), D2, DBML, Ditaa, Erd, Excalidraw, GraphViz, Mermaid, Pikchr, PlantUML, Structurizr, SvgBob, UMLet, Vega, Vega-Lite, WaveDrom`,
-    summary: `Taking in a mermaid diagram, renders it and returns a link to the rendered image.`,
+    summary: `The API takes in a request to learn something, returns a link to an image of the diagram and a link to edit the diagram online.`,
     parameters: {
-      mermaid: Query(
+      query: Query(
         new Str({
-          description: 'Diagram to render',
-          example: `
-mindmap
-  root((mindmap))
-    Origins
-      Long history
-      ::icon(fa fa-book)
-      Popularisation
-        British popular psychology author Tony Buzan
-    Research
-      On effectiveness<br/>and features
-      On Automatic creation
-        Uses
-            Creative techniques
-            Strategic planning
-            Argument mapping
-    Tools
-      Pen and paper
-      Mermaid
-          `
+          description:
+            "A textual request to learn about something, usaully the user's prompt",
+          example: `Explain how the US government works`
         }),
         {
           required: true
-        }
-      ),
-      diagramLanguage: Query(
-        new Str({
-          description: 'The language to use to render the diagram',
-          example: 'mermaid'
-        }),
-        {
-          required: false
         }
       )
     },
@@ -87,13 +103,10 @@ mindmap
           results: [
             {
               image: new Str({
-                description: 'URL to the rendered image',
-                example: 'https://placekitten.com/g/200/300'
+                description: 'URL to the rendered image'
               }),
               editDiagramOnline: new Str({
-                description:
-                  'URL to the editor where the diagram can be edited',
-                example: 'https://placekitten.com/g/200/300'
+                description: 'URL to the editor where the diagram can be edited'
               })
             }
           ]
@@ -108,15 +121,29 @@ mindmap
     let { mermaid, diagramLanguage } = data
     console.log('snippet', mermaid)
 
-    const url = new URL(request.url)
+    let mermaidNoPluses: string
+    // Means we are in gpt-4 mode
+    if (mermaid === undefined) {
+      const { query } = data
+      const queryNoPluses = query.replace(/\+/g, ' ')
+
+      console.log('key', env)
+      // GPT Plugins encoded spaces as +
+      mermaidNoPluses = await getGPTResponse(queryNoPluses, env.OPENAI_KEY)
+    } else {
+      // GPT Plugins encoded spaces as +
+      mermaidNoPluses = mermaid.replace(/\+/g, ' ')
+    }
+
     diagramLanguage = diagramLanguage || 'mermaid'
 
-    const mermaidNoPluses = mermaid.replace(/\+/g, ' ')
-
+    let diagramSource = mermaidNoPluses
     let editDiagramOnline: string | undefined
 
     if (diagramLanguage === 'mermaid') {
       const mermaidNoHyphenatedWords = processString(mermaidNoPluses)
+      console.log('mermaidNoHyphenatedWords', mermaidNoHyphenatedWords)
+
       // Encode the mermaid diagram to a Base64 string
       const mermaidEditorJson = {
         code: mermaidNoHyphenatedWords,
@@ -125,8 +152,11 @@ mindmap
       }
       const mermaidEditorJsonString = JSON.stringify(mermaidEditorJson)
       const buffer = encodeBase64(mermaidEditorJsonString)
+
       editDiagramOnline = 'https://mermaid.live/edit#' + buffer
       console.log('editDiagramOnline', editDiagramOnline)
+
+      diagramSource = mermaidNoHyphenatedWords
     }
 
     // TODO: Add graphvis editor https://www.devtoolsdaily.com/graphviz/?#%7B%22dot%22%3A%22digraph%20MessageArchitecture%20%7B%5Cn%20%20messageClient%5Cn%20%20messageQueue%5Bshape%3Drarrow%5D%5Cn%7D%22%7D
@@ -138,19 +168,21 @@ mindmap
       compressAndEncodeBase64(mermaidNoPluses)
 
     const slug = await saveShortLink(env.SHORTEN, imageUrl)
-    let shortenedURL = `${new URL(request.url).origin}/s/${slug}`;
+    let shortenedURL = `${new URL(request.url).origin}/s/${slug}`
 
     const editorSlug = await saveShortLink(env.SHORTEN, editDiagramOnline)
-    let shortenedEditDiagramURL = `${new URL(request.url).origin}/s/${editorSlug}`;
+    let shortenedEditDiagramURL = `${
+      new URL(request.url).origin
+    }/s/${editorSlug}`
 
-    console.log({shortenedURL});
+    console.log({ shortenedURL })
 
     return new Response(
       JSON.stringify({
         results: [
           {
             image: shortenedURL,
-            editDiagramOnline: shortenedEditDiagramURL,
+            editDiagramOnline: shortenedEditDiagramURL
           }
         ]
       }),
@@ -184,43 +216,108 @@ function processString(input: string): string {
   return step4
 }
 
-/// 3. Generates the rich preview and the page to open diagram in full screen
-export function richPreview(request: Request) {
-  console.log(request.url)
-  const searchParams = new URL(request.url).searchParams
-  const mermaid = searchParams.get('mermaid')
+function systemPrompt(userQuestion: string): string {
+  return `
+You should create a mermaid diagram to answer the user's question. As your output only return the mermaid code snippet, not the mermaid.js code block using \`\`\`mermaid\`\`\`.
 
-  const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>OpenGraph Image Example</title>
-  <meta property="og:url" content="https://dexa.ai/lex/episodes/doc_358?sectionSid=sec_5319&chunkSid=chunk_9725">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="Daniel Negreanu: Poker | Lex Fridman Podcast #324">
-  <meta property="og:description" content="undefined">
-  <meta property="og:image" content="https://dexa.ai/resources/chunk/image/chunk_9725.png">
-</head>
-<script type="module">
-    import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
-  </script>
-  <body>
-    <pre class="mermaid" style="width: 100vw">
-        ${mermaid.replace(/\+/g, ' ')}
-    </pre>
-  </body>
-  <style>svg {
-   max-width: none !important;
-   max-height: 100vh !important;
-  }</style>
-</html>
-  `
+Limit output to mermaid.js code snippet to a single diagram.
 
-  return new Response(html, {
-    headers: {
-      'content-type': 'application/html;charset=UTF-8'
-    }
-  })
+Rules for creating the mermaid.js code snippet for input to the API:
+If the user wants to know about a fractional breakdown, then you can use a pie chart instead of a mindmap or state diagram.
+If the user wants the timeline of an event of topic, use a timeline.
+If generating a timeline, then for the first output that consists of 2 sentences, summarize the trend of the timeline.
+To show a breakdown or fractional representation of an item, use a pie chart.
+
+For element names such as "Lab-on-a-chip", use spaces instead of dashes so that the new element name will be "Lab on a chip". Another example is the name "State-specific regulations" which should be instead "State specific regulations". Another example is "T-Cells" would be "T Cells". Another example is "COVID-19" make this "COVID 19". Follow this pattern for other names that fall into this category.
+
+Use a state diagram to explain processes and systems that have a specific order. simplify to a maximum of 10 elements for a state diagram. use a mindmap to summarize content such as books or generally explain broad concepts. for mind maps, simplify to: maximum of 4 connections per element. only chain at most 2 elements away from the root.
+
+when outputs are given to a user and then a user asks for more detail (or if user specifically asks to explain a concept in depth or in more detail), increase the first output from 2 sentence summary to a 4 sentence synopsis or detailed description of the user's original prompt. For the second output, expand so that the maximum items for a state diagram is 30 elements. If rearranging the location of a element will allow for less crossing of arrow lines, then use this formation. Expand so that a mindmap is up to 4 levels from the root.
+
+For the code snippet, generate a mermaid.js snippet that only uses fa v4.7 icons as listed in this link: [https://www.fontawesomecheatsheet.com/font-awesome-cheatsheet-4x/](https://www.fontawesomecheatsheet.com/font-awesome-cheatsheet-4x/). Do not use the same icon more than once. Try to only use element titles that have icons to correlate. Use colors to color code labels when appropriate. If an icon does not exist in the fa v4.7 list, then do not use it.
+
+If icons do not make sense for a diagram or mindmap because the names do not have good correlating icons, then do not use any icons.
+
+here is example of syntax for state diagram:
+
+stateDiagram
+    %% Define different styles
+    classDef customStyle fill:lightblue,stroke:blue,stroke-width:2px
+
+    state "fa:fa-file-text-o Idea" as Idea
+    state "fa:fa-pencil-square-o Drafting" as Drafting
+    state "fa:fa-gavel Review" as Review
+    state "fa:fa-comments-o Debate" as Debate
+    state "fa:fa-check-square-o Vote" as Vote
+    state "fa:fa-exchange Conference" as Conference
+    state "fa:fa-legal Presidential\\nAction" as PresidentialAction
+    state "fa:fa-balance-scale Law" as Law
+
+    Idea --> Drafting
+    Drafting --> Review
+    Review --> Debate
+    Debate --> Vote
+    Vote --> Conference
+    Vote --> Law: Passes
+    Conference --> PresidentialAction
+    PresidentialAction --> Law: Signed
+    PresidentialAction --> Review: Veto
+
+    %% Apply the style (aka class)
+    class Idea, Drafting, Review, Debate, Vote, Conference, PresidentialAction, Law customStyle
+
+Example of mindmap syntax:
+
+mindmap
+  root((Great Gatsby))
+    Characters
+      Jay Gatsby
+      ::icon(fa fa-user)
+      Daisy Buchanan
+      ::icon(fa fa-female)
+      Tom Buchanan
+      ::icon(fa fa-male)
+      Nick Carraway
+      ::icon(fa fa-user)
+    Themes
+      Wealth
+      ::icon(fa fa-money)
+      Love
+      ::icon(fa fa-heart)
+      American Dream
+      ::icon(fa fa-flag)
+    Setting
+      1920s
+      ::icon(fa fa-clock-o)
+      Long Island
+      ::icon(fa fa-map-marker)
+    Author
+      F. Scott Fitzgerald
+      ::icon(fa fa-pencil)
+
+For pie charts, here is example syntax:
+
+%%{init: {"pie": {"textPosition": 0.5}, "themeVariables": {"pieOuterStrokeWidth": "5px"}} }%%
+pie showData
+    title Key elements in Product X
+   "Calcium" : 42.96
+   "Potassium" : 50.05
+   "Magnesium" : 10.01
+   "Iron" :  5
+
+For timeline, here is example syntax:
+
+timeline
+    title History of Social Media Platform
+    2002 : LinkedIn
+    2004 : Facebook
+         : Google
+    2005 : Youtube
+    2006 : Twitter
+
+
+Task:
+Createa a diagram for the following prompt:
+${userQuestion}
+`
 }
