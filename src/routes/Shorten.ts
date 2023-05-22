90495ac21499b3644f314e2040afafabba5afdce
nobody <nobody@localhost>
2023-05-21 22:58:21 -0700

    fix #19 make old links work again

--- src/routes/Shorten.ts
+++ src/routes/Shorten.ts
@@ -21,16 +21,32 @@ export async function ShortLinkRoute(request, env) {
   if (!slug) {
     return new Response('404 Not Found...', { status: 200 })
   }
-  const targetUrl = await env.SHORTEN.get(slug)
-  if (!targetUrl) {
+
+  const data = await env.SHORTEN.get(slug)
+  if (!data) {
     return new Response('404 Not Found...', { status: 200 })
   }
 
-  return new Response(null, {
-    status: 301,
-    statusText: 'Moved Permanently',
+  // Here is the deal: There is a bug where we started passing the SVG
+  // contents to the shorten function expecting to get a link to this blob but
+  // then we also passed the editor links expecting to get redirects.
+  // Previously we always stored a redirect location. Fortunately all the SVG
+  // blobs don't start with http so we can fix this route in a backwards
+  // compatible way. If we were using a new type wrapper so that only URLs
+  // could be passed to saveShortLink then this problem would have been avoided.
+  if (data.startsWith('http')) {
+    return new Response(null, {
+      status: 301,
+      statusText: 'Moved Permenantly',
+      headers: {
+        'Location': data,
+      }
+    });
+  }
+  // Assume that the all the non link data is SVG files
+  return new Response(data, {
     headers: {
-      Location: targetUrl
+      'content-type': 'image/svg+xml'
     }
   })
 }
