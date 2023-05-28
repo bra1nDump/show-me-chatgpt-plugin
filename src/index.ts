b551c3c817b879a90fb3ad969622297a2268fc48
Oscar <odrr.99@gmail.com>
2023-05-28 11:36:06 -0500

    feat: improve prompt engineering when rendering diagram and exploring other types of diagrams

--- src/index.ts
+++ src/index.ts
@@ -149,25 +149,25 @@ Examples of user prompts in which you should use this plugin:
 
 Use this plugin if a user asks about: how something works, explain about a concept or how something is made, summarize a book, describe a process, and other user prompts that can be summarized or explained in the format of a mindmap or a state diagram. Also use this plugin if the user asks to show a ratio or fraction of something, for example "show me the ideal breakdown for budgeting" or "what is the ratio of political parties in the senate?". Also use this plugin if the user asks to show or draw a timeline of an event or topic.
 
-Before rendering diagrams to the plugin API:
-Only if it is the first time rendering a diagram get list of supported diagrams, otherwise do not request.
-
 Rendering diagrams to the plugin API:
+Before rendering diagrams if it is the first time rendering diagrams then get the list of supported diagrams, otherwise do not request. After this you should say the type and language of the diagram you will render. Then you start rendering the diagram.
 If an guidelines is included in the response from the information endpoint, make sure to follow them.
 
 Examples: 
 User asks: "Show me how vscode internals work."
-You request guidelines: graph_mermaid
-You follow guidelines and start rendering diagram
+1. You say "I will render a graph diagram in mermaid"
+2. You request both getSupportedDiagrams and getDiagramGuidelines(graph_mermaid) from the information endpoint
+3. You start rendering diagram by using the render endpoint following the guidelines
 
-User asks: "Show me how a food critic can interact with a restaurant"
-You request guidelines: plantuml_use-case
-You follow guidelines and start rendering diagram
+After this the user asks: "Show me how a food critic can interact with a restaurant"
+1. You say "I will render a use case diagram in plantuml"
+2. You do not request getSupportedDiagrams because you already know it, but you request getDiagramGuidelines(plantuml_use-case) from the information endpoint
+3. You start rendering diagram by using the render endpoint following the guidelines
 
 Interpreting the API response:
 - When you get the response it will include an image url, you should render it inline using ![alt text](image) syntax, also you should give the user a link to open it in a new tab saying: [You can view this diagram in a new tab.](editDiagramOnline)
 - If you get a link at editDiagramOnline to edit the diagram then you should render it inline using [link text](editDiagramOnline) syntax otherwise say "The link to edit this diagram online is not implemented yet". When including the suggestion to edit the diagram, ensure it is part of the link text. For example: [You can edit this diagram online if you want to make any changes.](editDiagramOnline).
-- You should give the user a link to open the diagram type documentation from the "List of supported diagram languages and diagram types", say: [You can access the diagram documentation to unveil the capabilities of the <<diagram-language>> <<diagram-type>>.](documentationLink).
+- You should give the user a link to open the diagram type documentation from the "List of supported diagram languages and diagram types", say: [You can access the diagram documentation of the <<diagram-language>> <<diagram-type>>.](documentationLink).
 - You should create the response in that order: first the image, then suggestion to edit using works, then the open new tab link, then the edit link, then the documentation link, then the textual explanation, then conclude with the recommendations section.
 
 Recommendation section:
@@ -177,7 +177,11 @@ The section has two parts: suggestions to improve the rendered diagram and explo
 First, introduce the section with the message like "### Here are ways to improve the diagram to provide more detail or clarity:". Second, provide a unordered list of between 3 and 4 suggestions, the items follows a pattern "**{reason}**: {explanation}".  
 
 2. Explore other types of diagrams
-First, introduce the section with the message like "### Here are some other types of diagrams that could be used to render the same concept:". Second, provide a unordered list between 3 to 4 items of alternative diagram types that could be used to render the same concept. Do not suggest types of diagrams outside of the "List of supported diagram languages and diagram types". The items follows a pattern "**{reason}**: {explanation}"
+First, introduce the section with the message like "### Here are some other types of diagrams that could be used to render the same concept:". 
+
+Second, provide a unordered list between 3 to 4 items of alternative diagram types that could be used to render the same concept. Do not suggest types of diagrams outside of the "List of supported diagram languages and diagram types". The items follows a pattern "**{reason}**: {explanation}". Include all the diagram languages of the same diagram type at the end of the explanation, for example: "{explanation}... consider using (plantuml)[diagramTypeDocumentationLink], (mermaid)[diagramTypeDocumentationLink] or (d2)[diagramTypeDocumentationLink])" languages. Get diagramTypeDocumentationLink only from the "List of supported diagram languages and diagram types", do not use diagram languages outside of the list of supported diagram languages at the end of the explanation, for example the use case diagram should suggest only plantuml and do not suggest user journey diagram.
+
+Third, say to the user "You can experiment with different languages for <<diagramType>> diagram such as (<<diagramLanguage>>)[<<diagramTypeDocumentationLink>>]".
 
 Tips:
 - You will also get a link contributeToOpenSourceProject. You should suggest the user contributes to the project if and only if:
